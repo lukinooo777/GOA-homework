ccebcda1b03adbe87870fb8ace6fc3e6738744f7
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-11 23:31:02 +0400

    second commit

---
+++ day 2/lesson 2.py
@@ -0,0 +1,25 @@
+name = "luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიწებელი სიმბოლო
+#  "luka" არის ცვლადისთვის მნიშვნელობა
+surname="khurtsidze"
+ #print(name)
+#პრინტ ფუნქცია გადაეცემა ეკრანზე გამოსატანი ობიექტი
+name= "luka"  #ეს არის str (string) ტიპის ცვლადი
+height= 184.5  #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean  (bool) ტიპის ცვლადი
+knows_programming=True
+is_ugly=False
+isUgly=False # javascript  camelcase
+print(name + " "  +  surname)
+age=22
+print(name + str(age))
+
+#print(type)(height))
+#print(type(programming))
+
+
+name="luka"
+surname="khurtsidze"
+age=14
+print(name + " " + surname + " "  + str(age)  +  " "  +  str(height))
