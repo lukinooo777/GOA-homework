64f1f7952772b01628d03b6e5db42dcbb59153e6
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-14 21:06:53 +0400

    second commit

---
+++ day 02/class/index.py
@@ -0,0 +1,30 @@
+# print("hellow world")  #string
+
+#print გამოიყენება იმისთვის რომ გამოვიტანოთ ტერმინალში კოდი
+
+# print(110) #intenger
+
+#text =  string   this is exeple of string
+
+#number  = intenger  - 10, 20, 35, ....
+
+  
+# +   მიმატება
+#- გამოკლება
+#* გამრავლება
+#/-//  გაყოფა
+#% - ნაშთის გამოსათვლელად
+
+
+print(10 + 10)
+print(70-40)
+print(15*10)
+print(100/5)
+print(7 // 3)
+print(13 / 4)
+
+
+print("face" + "book")
+print("dis"+ "cord")
+#როდესაც  სიტყვებს ერთმანეთზე ვამატებთ მაგტას ეწოდება  კონკატინაცია
+print("hellow world my name is luka I'm from georgia")
