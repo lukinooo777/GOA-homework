612831a2646d91d5c097e64f2afb78a20888e2e0
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-15 20:59:22 +0400

    second commit

---
+++ day 03/day 03.py/class.py
@@ -0,0 +1,19 @@
+# ცვლადი შედგება
+# 1 ცვლადი შედგება სახელებისგან
+# 2 დამაკავშირებელი სიმბოლოთი (=)
+# 3 ცვლადის მნიშვნელობა
+name = "luka"
+print(name)
+last_name = "xurcidze"
+country = "Georgia"
+age = "13"
+print(last_name)
+print(country)
+print(age)
+print(name, last_name, country, age)
+print("hellow my name is" + " " + name +    "my last_name is" + " " + last_name +  "I am from" + " " + country)
+x = 15
+y = 16
+print(x + y)
+z = x + y
+print = z
