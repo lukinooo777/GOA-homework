c64cb38dc007a9f313f527b5b5649ed4d2417582
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-20 20:15:10 +0400

    second commit

---
+++ day 07/homework/home.py
@@ -0,0 +1,27 @@
+name = input('Enter your name: ')
+last_name = input('Enter your last_name:')
+age = int(input('Enter your age: '))
+country = input ('Enter your country: ')
+city = input ('Enter your city :') 
+fav_car = input('enter your fav car ') 
+
+
+
+print("Hellow my name is " + name + " " + "my last_name is" +   last_name + " " + "l am " + str(age) + "years old " "I am from" +    country + " "  "I live in " + city + " " "my favourite car is " + " " +  fav_car)
+  
+
+
+  #int შეუძლია str შეცვალოს ინტეგერრად
+  #str 
+  #type ჩვენ  შეგვიძლია გავიგოთ თუ რა ცვლადისაა წინადადება
+  #input  ჩვენ ინ პუთის დახმარებიტ ინფორმაცია შეგვაქ მოწყობილობაშ
+   
+     
+
+x = int(input("Enter x:"))
+y = int(input("Enter y:"))
+
+print(x + y)
+
+print(x - y)
+
