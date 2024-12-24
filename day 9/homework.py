10b3a6044fa95a8c8adfe9df3d68c148ef61a29b
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-25 01:59:55 +0400

    second commit

---
+++ day 9/homework.py
@@ -0,0 +1,23 @@
+
+print(10 > 5)
+print(12 < 27)
+print(13 >= 13)
+print( 34 != 30)
+print(200 == 200)
+
+age= str(input("enter your age"))
+print(type(age))
+
+
+
+
+
+name = input("Enter your name")
+password = input("enter your password")
+
+
+
+print(True  and  False)
+print(True or False)
+
+
