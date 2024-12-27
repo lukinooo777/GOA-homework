c5091637b70a6151378670c07bb1a89359421f60
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-28 01:09:41 +0400

    second commit

---
+++ day 11/homework/homework.py
@@ -0,0 +1,20 @@
+
+age = 23 
+
+if age < 13:
+    print("child")
+
+elif age  > 13 and age < 17:
+    print("teenager")
+elif age > 20:
+        print("adult")
+
+
+number = 3
+
+if number is 0:
+   print("zero")
+elif number > 0:
+   print("positive number")
+elif number < 0:
+   print("negative number")
