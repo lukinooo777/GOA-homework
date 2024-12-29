670ad1003e32a0eeaf6dc537996f82c7e0469f67
lukinooo777 <lukaxurcidze381@gmail.com>
2024-12-30 01:10:07 +0400

    second commit

---
+++ day 13/homework/homework.py
@@ -0,0 +1,14 @@
+
+number= 100
+  while number > 0:
+ print(number)
+number -= 5
+
+password =int(input("Enter your password:")
+
+  while password == ":"
+ print("your password is incorrect")
+password =  int(input("Enter your password:")
+
+print( f"hellow {password}")
+
