d26881296289937289c27f8888b18c07d56d3340
lukinooo777 <lukaxurcidze381@gmail.com>
2025-01-02 04:04:07 +0400

    second commit

---
+++ day 15/homework.py
@@ -0,0 +1,25 @@
+
+number = int(input("Enter your number"))
+
+if number > 0:
+    print("positive")
+elif number < 0:
+    print("negative")
+
+
+score = int(input("Enter your score"))
+if score  > 90:
+     print("your grade is A") 
+elif score > 80 < 90:
+     print("your grade is B")
+elif score > 70 < 80:
+      print("your grade is C")
+elif score < 70:
+       print("your grade is D")     
+
+
+
+
+year = int(input("Enter your year"))
+if  year // 4:
+    print("this year is nakiani")
